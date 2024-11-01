1209d3eef75311ae0d1f1f3c0afe2b1f985da905
achul123 <73477238+achul123@users.noreply.github.com>
2024-11-01 18:36:15 +0100

    First init

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+// ╳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╳
+//      AirLink - Open Source Project by AirlinkLab
+//      Repository: https://github.com/airlinklab/airlink
+//
+//      © 2024 AirlinkLab. Licensed under the MIT License
+// ╳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╳
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
