b035b88c250a664c9ed3575c12867e4a2e5cb243
privt00 <noahunterweger3@gmail.com>
2024-11-02 23:29:09 +0100

    a

--- src/__tests__/routes/getrouters.test.ts
+++ src/__tests__/routes/getrouters.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../../app'; // Pfad zu deiner Express-App
+import app from '../../app';
 
 describe('GET /', () => {
   it('should respond with a 200 status code', async () => {
