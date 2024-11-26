2e9f58d019375a1b1c8839d705c583d575a08b9d
privt00 <noahunterweger3@gmail.com>
2024-11-26 18:28:41 +0100

    Better search bar

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -36,7 +36,9 @@ function filterLinks(searchTerm) {
     filteredLinks.forEach((link, index) => {
       const resultLink = document.createElement('a');
       resultLink.href = link.href;
-      resultLink.textContent = link.textContent;
+      resultLink.onclick = () => {
+        location.href = link.href;
+      };
       resultLink.classList.add(
         'nav-link',
         'transition',
@@ -54,7 +56,7 @@ function filterLinks(searchTerm) {
         'font-medium',
         'rounded-xl',
         'hover:border',
-        'hover:border-neutral-800/20',
+        'hover:border-neutral-800/20'
       );
 
       if (index === 0) {
@@ -66,13 +68,79 @@ function filterLinks(searchTerm) {
           'searchLinkActive',
           'mt-4',
           'border',
-          'border-neutral-800/20',
+          'border-neutral-800/20'
         );
       }
+
+      const originalIcon = link.querySelector('svg');
+      let icon;
+      if (originalIcon) {
+        icon = originalIcon.cloneNode(true);
+        icon.classList.add('w-5', 'h-5', 'mr-2');
+      }
+
+      const linkText = document.createElement('span');
+      linkText.textContent = link.textContent;
+
+      const breadcrumbBadge = document.createElement('span');
+      breadcrumbBadge.classList.add(
+        'breadcrumb',
+        'bg-gray-200',
+        'text-gray-600',
+        'rounded-md',
+        'ml-2',
+        'text-xs',
+        'px-2',
+        'py-1'
+      );
+      breadcrumbBadge.innerHTML = getBreadcrumbWithHomeIcon(link.href);
+
+      if (icon) resultLink.appendChild(icon);
+      resultLink.appendChild(linkText);
+      resultLink.appendChild(breadcrumbBadge);
 
       searchResults.appendChild(resultLink);
     });
   }
+}
+
+function getBreadcrumbWithHomeIcon(href) {
+  const url = new URL(href);
+  const pathParts = url.pathname.split('/').filter(Boolean);
+
+  const homeIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
+
+  `;
+
+  const arrowIcon = `
+    <svg class="h-5 w-5 -mr-2 -ml-2 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+      <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
+    </svg>
+  `;
+
+  if (pathParts.length > 0) {
+    const breadcrumb = pathParts
+      .map(
+        (part) => `
+          <span class="flex items-center">
+            ${arrowIcon}
+            <span class="ml-2">${part}</span>
+          </span>
+        `
+      )
+      .join('');
+    return `
+      <div class="flex items-center space-x-2">
+        <span>${homeIcon}</span>
+        ${breadcrumb}
+      </div>
+    `;
+  }
+
+  return homeIcon;
 }
 
 filterLinks('');
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     './src/**/*.{html,ejs,js,ts}',
     './views/**/*',
+    './public/**/*'
   ],
   darkMode: 'class',
   theme: {
